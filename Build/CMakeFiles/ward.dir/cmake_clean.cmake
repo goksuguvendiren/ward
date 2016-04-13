febe9d5a23c562d3b404cad3793eea108cde02da
Goksu Guvendiren <goksuguvendiren@gmail.com>
2016-04-14 00:30:55 +0300

    updating structure

---
+++ Build/CMakeFiles/ward.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ward.dir/Source/main.cpp.o"
+  "CMakeFiles/ward.dir/Source/aoa_jpeg.cpp.o"
+  "ward.pdb"
+  "ward"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ward.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
