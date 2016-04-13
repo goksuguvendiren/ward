4953992752d1be26a14f00e86f91e58057fbb364
Goksu Guvendiren <goksuguvendiren@gmail.com>
2016-04-14 00:31:41 +0300

    updating structure

---
+++ Header/aoa_jpeg.h
@@ -0,0 +1,10 @@
+#ifndef __aoa_jpeg_h__
+#define __aoa_jpeg_h__
+
+typedef float UCOLOR[3];
+
+void write_jpeg(const char *filename, UCOLOR **image, int width, int height);
+void read_jpeg(const char *filename, UCOLOR **image, int *width, int *height);
+void read_jpeg_header(const char *filename, int *width, int *height);
+
+#endif //__aoa_jpeg_h__
